ca6111e98f9d08048cbc56bb15e9bf3397c66ab6
Imran Abdulmalik <isoteriksoftware@gmail.com>
2021-06-18 20:37:33 +0100

    added tests for AnimationStateMachine

---
+++ src/test/java/io/github/isoteriktech/xgdx/animation/AnimationStateMachineTest.java
@@ -0,0 +1,149 @@
+package io.github.isoteriktech.xgdx.animation;
+
+import com.badlogic.gdx.ai.fsm.State;
+import com.badlogic.gdx.ai.msg.Telegram;
+import io.github.isoteriktech.xgdx.animation.conditions.BooleanCondition;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AnimationStateMachineTest {
+    State stateA = new State() {
+        @Override
+        public void enter(Object entity) {
+
+        }
+
+        @Override
+        public void update(Object entity) {
+
+        }
+
+        @Override
+        public void exit(Object entity) {
+
+        }
+
+        @Override
+        public boolean onMessage(Object entity, Telegram telegram) {
+            return false;
+        }
+    };
+    State stateB = new State() {
+        @Override
+        public void enter(Object entity) {
+
+        }
+
+        @Override
+        public void update(Object entity) {
+
+        }
+
+        @Override
+        public void exit(Object entity) {
+
+        }
+
+        @Override
+        public boolean onMessage(Object entity, Telegram telegram) {
+            return false;
+        }
+    };
+    State stateC = new State() {
+        @Override
+        public void enter(Object entity) {
+
+        }
+
+        @Override
+        public void update(Object entity) {
+
+        }
+
+        @Override
+        public void exit(Object entity) {
+
+        }
+
+        @Override
+        public boolean onMessage(Object entity, Telegram telegram) {
+            return false;
+        }
+    };
+
+    @Test
+    public void currentStateTest() {
+        BooleanCondition booleanCondition = new BooleanCondition(false);
+
+        Transition transition = new Transition(stateA, stateB, booleanCondition);
+
+        AnimationStateMachine stateMachine = new AnimationStateMachine();
+
+        stateMachine.setInitialState(stateA);
+        stateMachine.addTransition(transition);
+        stateMachine.update();
+
+        assertSame(stateA, stateMachine.getCurrentState());
+
+        booleanCondition.value.set(true);
+        stateMachine.update();
+
+        assertSame(stateB, stateMachine.getCurrentState());
+    }
+
+    @Test
+    public void multipleTransitionTest(){
+        BooleanCondition booleanCondition1 = new BooleanCondition(false);
+        BooleanCondition booleanCondition2 = new BooleanCondition(true);
+
+        Transition transitionAtoB = new Transition(stateA, stateB, booleanCondition1);
+        Transition transitionBtoA = transitionAtoB.invert();
+        Transition transitionBtoC = new Transition(stateB, stateC, booleanCondition2);
+
+        AnimationStateMachine stateMachine = new AnimationStateMachine();
+
+        stateMachine.setInitialState(stateA);
+        stateMachine.addTransitions(transitionAtoB, transitionBtoA, transitionBtoC);
+        stateMachine.update();
+
+        assertSame(stateA, stateMachine.getCurrentState());
+
+        // Intent: transition from A to B
+        booleanCondition1.value.set(true);
+        stateMachine.update();
+
+        /*
+         What should happen: the current state should be C, and not B because the transition to C from B is triggered.
+         Since that transition comes last, it will win.
+         Note: the transition from A to B is also triggered but it will not be the final used transition because the next
+         transition is also triggered.
+        */
+        assertSame(stateC, stateMachine.getCurrentState());
+
+        // Intent: transition from C to B
+        booleanCondition2.value.set(false);
+        stateMachine.addTransition(transitionBtoC.invert()); // to make the transition bi-directional
+        stateMachine.update();
+
+        assertSame(stateB, stateMachine.getCurrentState());
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/test/java/io/github/isoteriktech/xgdx/animation/TransitionTest.java
@@ -0,0 +1,101 @@
+package io.github.isoteriktech.xgdx.animation;
+
+import com.badlogic.gdx.ai.fsm.State;
+import com.badlogic.gdx.ai.msg.Telegram;
+import io.github.isoteriktech.xgdx.animation.conditions.BooleanCondition;
+import io.github.isoteriktech.xgdx.animation.conditions.NumericCompoundCondition;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TransitionTest {
+    State stateA = new State() {
+        @Override
+        public void enter(Object entity) {
+
+        }
+
+        @Override
+        public void update(Object entity) {
+
+        }
+
+        @Override
+        public void exit(Object entity) {
+
+        }
+
+        @Override
+        public boolean onMessage(Object entity, Telegram telegram) {
+            return false;
+        }
+    };
+    State stateB = new State() {
+        @Override
+        public void enter(Object entity) {
+
+        }
+
+        @Override
+        public void update(Object entity) {
+
+        }
+
+        @Override
+        public void exit(Object entity) {
+
+        }
+
+        @Override
+        public boolean onMessage(Object entity, Telegram telegram) {
+            return false;
+        }
+    };
+
+    @Test
+    public void triggerTest() {
+        NumericCompoundCondition numericCompoundCondition = new NumericCompoundCondition(3f);
+        numericCompoundCondition.greaterThan(2f)
+                .lessThan(4f)
+                .greaterThan(1.5f);
+
+        Transition transition = new Transition(stateA, stateB, numericCompoundCondition);
+
+        assertTrue(transition.isTriggered());
+
+        numericCompoundCondition.setValue(0f);
+        assertFalse(transition.isTriggered());
+    }
+
+    @Test
+    public void reversedTransitionTest() {
+        BooleanCondition booleanCondition = new BooleanCondition(false);
+
+        Transition transition = new Transition(stateA, stateB, booleanCondition);
+
+        assertFalse(transition.isTriggered());
+
+        Transition reversedTransition = transition.invert();
+        assertTrue(reversedTransition.isTriggered());
+
+        assertSame(stateA, reversedTransition.to);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

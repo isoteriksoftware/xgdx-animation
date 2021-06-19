598f73b54f97d675da8a70b722d0910dc74ad51a
Imran Abdulmalik <isoteriksoftware@gmail.com>
2021-06-19 07:09:12 +0100

    added test assets, lwgl3 and updated to use java 8

--- runtime-test/desktop/src/io/github/isoteriktech/xgdx/animation/test/desktop/DesktopLauncher.java
+++ runtime-test/desktop/src/io/github/isoteriktech/xgdx/animation/test/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
 package io.github.isoteriktech.xgdx.animation.test.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import io.github.isoteriktech.xgdx.animation.test.XgdxAnimationTest;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new XgdxAnimationTest(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new XgdxAnimationTest(), config);
 	}
 }
